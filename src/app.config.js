69fa3b349f51bd24e9c42ee9c7cd2e7abc41421c
jvegahub <johnvega@deloitte.com>
2022-11-03 16:47:40 +0000

    change made

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    team = 'Group 6';
+    team = 'Group 6 Reformed';
     version = '1.0.0';
 }
 module.exports = AppConfig;
